cf0adda42378d053f2e306b8f2bf76095fe01d1f
Leonardo MUNOZ QUINTERO <lmunoz-q@e3r12p8.42.fr>
2018-11-21 17:09:56 +0100

    rm dossiers useless

--- src/parsing/default_map.c
+++ src/parsing/default_map.c
@@ -6,7 +6,7 @@
 /*   By: hmartzol <hmartzol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/14 13:18:52 by hmartzol          #+#    #+#             */
-/*   Updated: 2018/09/14 15:46:31 by lmunoz-q         ###   ########.fr       */
+/*   Updated: 2018/11/21 17:07:56 by lmunoz-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ t_header	*default_map(void)
 	uint32_t				i;
 	t_header				*out;
 	static const t_header	head = {
-		.magic = W3D_MAGIC,
-		.height = 11,
-		.width = 11,
-		.startx = 5,
-		.starty = 5,
-		.look = 0
-	};
+
+	.magic = W3D_MAGIC,
+	.height = 11,
+	.width = 11,
+	.startx = 5,
+	.starty = 5,
+	.look = 0};
 	if ((out = malloc(sizeof(t_header) + 121 * sizeof(uint32_t))) == NULL)
 		return (NULL);
 	*out = head;
--- src/render/shaders/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 400 core
-in vec2 texture_coordinates;
-
-uniform sampler2D _texture;
-
-void main()
-{
-	gl_FragColor = texture(_texture, texture_coordinates);
-}
