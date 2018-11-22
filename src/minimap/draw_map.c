4b4217c580e0114650f0469599892090d694b80a
LIMachi <lee.mach.hm@gmail.com>
2018-11-22 18:56:33 +0100

    quick fix pixel bleed

--- src/minimap/draw_map.c
+++ src/minimap/draw_map.c
@@ -17,9 +17,9 @@ t_glfw_window	*draw_pixel(t_glfw_window *win, uint32_t x, uint32_t y, uint32_t c
 {
 	if (win == NULL || x >= win->vb_width || y >= win->vb_height)
 		return (win);
-	win->vb[x + y * win->vb_width * 3] = (color >> 16) & 0xFF;
-	win->vb[x + y * win->vb_width * 3 + 1] = (color >> 8) & 0xFF;
-	win->vb[x + y * win->vb_width * 3 + 2] = color & 0xFF;
+	win->vb[(x + y * (win->vb_width + 1)) * 3] = (color >> 16) & 0xFF;
+	win->vb[(x + y * (win->vb_width + 1)) * 3 + 1] = (color >> 8) & 0xFF;
+	win->vb[(x + y * (win->vb_width + 1)) * 3 + 2] = color & 0xFF;
 	return (win);
 }
 
@@ -48,11 +48,12 @@ t_glfw_window	*draw_map(t_glfw_window *win, t_header *map)
 	x = -1;
 	sx = (double)win->vb_width / (double)map->width;
 	sy = (double)win->vb_height / (double)map->height;
+	draw_square(win, (t_vec){.x = 10, .y =10}, (t_vec){.x = 10, .y = 10}, 0xFF0000);
 	while (++x < map->width)
 	{
 		y = -1;
 		while (++y < map->height)
-			draw_square(win, (t_vec){(uint32_t)(x * sx), (uint32_t)(y * sy)}, (t_vec){(uint32_t)sx, (uint32_t)sy}, map->map[x + y * map->width] ? 0xFFFFFF : 0);
+			draw_square(win, (t_vec){(uint32_t)(x * sx), (uint32_t)(y * sy)}, (t_vec){(uint32_t)sx, (uint32_t)sy}, map->map[x + y * map->width] ? 0 : 0xFFFFFF);
 	}
 	return (win);
 }
